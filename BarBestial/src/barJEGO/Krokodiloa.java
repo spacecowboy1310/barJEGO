7bbce8705c2de1e7b84aa7853875c46d96ace389
Gorka <Gorka@10.109.60.23>
2018-04-11 13:58:44 +0200

    ff

--- BarBestial/src/barJEGO/Krokodiloa.java
+++ BarBestial/src/barJEGO/Krokodiloa.java
@@ -3,7 +3,7 @@ package barJEGO;
 public class Krokodiloa extends Karta{
 
 	public Krokodiloa(int pId,int pKolorea) {
-		super("Krokodiloaa",pId,pKolorea,(""),true,10,new AurreratuBat(),null,new KaleratuJan());
+		super("Krokodiloa",pId,pKolorea,(""),true,10,new AurreratuBat(),null,new KaleratuJan());
 	}
 	
 	public void animalada(){
